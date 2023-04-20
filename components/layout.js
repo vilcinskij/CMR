0e5b9da4b5945983433a8d042c2461f7699856cb
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-04-20 22:53:12 +0300

    ad layout props

---
+++ components/layout.js
@@ -0,0 +1,16 @@
+import Footer from "./footer";
+import Header from "./header";
+
+
+const Layout = ({ children }) => {
+    return (
+
+        <div className="content">
+            <Header />
+            {children}
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Header from '@/components/header'
-import Footer from '@/components/footer'
 import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,10 +9,8 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div>
-      <Header />
       <h1>CMR generator</h1>
       <Link href='burgers'>ALL BURGERRS</Link>
-      <Footer />
     </div>
   )
 }
