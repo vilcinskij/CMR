e9a58ed80c34884e7bf2758c4c51189d5a8f40da
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-04-21 01:26:30 +0300

    fire animation font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Layout from '@/components/layout'
 import '@/styles/globals.css'
+// import 'bootstrap/dist/css/bootstrap.css'
 
 export default function App({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
@@ -9,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>CMR generator</h1>
+      <h1 className={`${styles.title} font-effect-fire-animation`}>CMR generator</h1>
       <Link href='burgers'>ALL BURGERRS</Link>
     </div>
   )
