b787e4ba0f7a53443df6ce2ca540210193e22528
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-04-20 23:04:01 +0300

    Muravey stiles

--- components/footer.js
+++ components/footer.js
@@ -3,8 +3,7 @@ import { AiOutlineCopyrightCircle } from 'react-icons/ai'
 const Footer = () => {
     return (
         <footer>
-            Copyright 2023 Truckburgerr
-            <AiOutlineCopyrightCircle />
+            Copyright 2023 <AiOutlineCopyrightCircle /> Vilcinskij Limited
         </footer>
     )
 }
