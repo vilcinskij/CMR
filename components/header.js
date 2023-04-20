725e6883037f206371b85e5ddd3102530f66c78b
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-04-20 20:52:59 +0300

    react-icon added

---
+++ components/header.js
@@ -0,0 +1,13 @@
+import {HiTruck} from 'react-icons/hi'
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+
+            </div>
+        </header>
+    )
+}
+
+export default Header;
