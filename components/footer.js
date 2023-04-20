947f7dfe9ce8824c5ad2103f998885125c5efef6
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-04-20 21:11:06 +0300

    header-footer import done

---
+++ components/footer.js
@@ -0,0 +1,12 @@
+import { AiOutlineCopyrightCircle } from 'react-icons/ai'
+
+const Footer = () => {
+    return (
+        <footer>
+            Copyright 2023 Truckburgerr
+            <AiOutlineCopyrightCircle />
+        </footer>
+    )
+}
+
+export default Footer
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>CMR generator</h1>
+      <Footer />
     </div>
   )
 }
