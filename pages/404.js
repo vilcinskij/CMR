6e21a95bcdd48f27fca1ff85b73a3c4fcd6ee119
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-04-22 00:35:37 +0300

    page 404

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const NotFoundPage = () => {
+    return (
+        <div>
+            PAGER NOT FOUNDERR
+            Go to <Link href='/burgers'> ALL burgers</Link>
+        </div>
+    )
+}
+
+export default NotFoundPage
