f1028eddeaa1a76c7f66241b4eb800e779b4cca6
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-04-22 13:59:23 +0300

    line-logic start

--- pages/cmr.js
+++ pages/cmr.js
@@ -3,26 +3,33 @@ import styles from '../styles/Cmr.module.css';
 const Cmr = () => {
     return (
         <div className={styles.mainCmrBlank}>
-            <div className={styles.f01t05logof16t18}>
-                <div className={styles.f01t05}>
-                    <div className={styles.f01t02}>
-                        <div className={styles.c01}></div>
-                        <div className={styles.c02}></div>
-                    </div>
-                    <div className={styles.f03t05}>
-                        <div className={styles.c03}></div>
-                        <div className={styles.c04}></div>
-                        <div className={styles.c05}></div>
-                    </div>
+                <div className={styles.c01}>1</div>
+                <div className={styles.c02}>2</div>
+                <div className={styles.c03}>3</div>
+                <div className={styles.c04}>4</div>
+                <div className={styles.c05}>5</div>
 
-                </div>
                 <div className={styles.logof16t18}>
-                    <div className={styles.logo}></div>
-                    <div className={styles.c16}></div>
-                    <div className={styles.c17}></div>
-                    <div className={styles.c18}></div>
+                    <div className={styles.logo}>logo</div>
+                    <div className={styles.c16}>16</div>
+                    <div className={styles.c17}>17</div>
+                    <div className={styles.c18}>18</div>
                 </div>
+
+            <div className={styles.description}>
+                <div className={styles.c6_9}>6-9</div>
+                <div className={styles.c10}>10</div>
+                <div className={styles.c11}>11</div>
+                <div className={styles.c12}>12</div>
+
             </div>
+            <div className={styles.l13_19}>L13_19</div>
+            <div className={styles.l14}>L14</div>
+            <div className={styles.l15_20}>L15_20</div>
+            <div className={styles.l21 - 24}>L21-24</div>
+            <div className={styles.l25_27}>L25_27</div>
+            <div className={styles.l28}>L28</div>
+            <div className={styles.l29}>L29</div>
         </div>
     )
 }
