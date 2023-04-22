92ba68af367756c60fa81a081ae3ccd885daa28a
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-04-22 15:42:44 +0300

    grid_done

--- pages/cmr.js
+++ pages/cmr.js
@@ -3,15 +3,57 @@ import styles from '../styles/Cmr.module.css';
 const Cmr = () => {
     return (
         <div className={styles.mainCmrBlank}>
-            <div className={styles.l1_logo}>L1_logo</div>
-            <div className={styles.l2_16}>L2_16</div>
-            <div className={styles.l3_18}>L3_18</div>
-            <div className={styles.l6_12}>L6_12</div>
-            <div className={styles.l13_19}>L13_19</div>
-            <div className={styles.l14}>L14</div>
-            <div className={styles.l15_20}>L15_20</div>
-            <div className={styles.l21 - 24}>L21-24</div>
-            <div className={styles.l25_27}>L25_27</div>
+            <div className={styles.l01_logo}>
+                <div className={styles.b01}>b01</div>
+                <div className={styles.blogo}>blogo</div>
+            </div>
+            <div className={styles.l02_16}>
+                <div className={styles.b02}>b02</div>
+                <div className={styles.b16}>b16</div>
+            </div>
+            <div className={styles.l03_18}>
+                <div className={styles.b03_05}>
+                    <div className={styles.b03}>b03</div>
+                    <div className={styles.b04}>b04</div>
+                    <div className={styles.b05}>b05</div>
+                </div>
+                <div className={styles.b17_18}>
+                    <div className={styles.b17}>b17</div>
+                    <div className={styles.b18}>b18</div>
+                </div>
+            </div>
+            <div className={styles.l06_12}>
+                <div className={styles.b06_09}>b06_09</div>
+                <div className={styles.b10}>b10</div>
+                <div className={styles.b11}>b11</div>
+                <div className={styles.b12}>b12</div>
+            </div>
+            <div className={styles.l13_19}>
+                <div className={styles.b13}>b13</div>
+                <div className={styles.b19}>b19</div>
+            </div>
+            <div className={styles.l14}>
+                <div className={styles.b14}>b14</div>
+            </div>
+            <div className={styles.l15_20}>
+                <div className={styles.b15}>b15</div>
+                <div className={styles.b20}>b20</div>
+            </div>
+            <div className={styles.l21_24}>
+                <div className={styles.b21_23}>
+                    <div className={styles.b21}>b21</div>
+                    <div className={styles.b22_23}>
+                        <div className={styles.b21}>b22</div>
+                        <div className={styles.b21}>b23</div>
+                    </div>
+                </div>
+                <div className={styles.b24}>b24</div>
+            </div>
+            <div className={styles.l25_27}>
+                <div className={styles.b25}>b25</div>
+                <div className={styles.b26}>b26</div>
+                <div className={styles.b27}>b27</div>
+            </div>
             <div className={styles.l28}>L28</div>
             <div className={styles.l29}>L29</div>
         </div>
