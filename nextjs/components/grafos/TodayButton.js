7fdd113b637a10281c8abd4c10bdecfa1b39786b
vilcinskij <82049094+vilcinskij@users.noreply.github.com>
2023-05-09 22:10:14 +0300

    buttonOnClick

---
+++ nextjs/components/grafos/TodayButton.js
@@ -0,0 +1,12 @@
+export default function TodayButton() {
+
+    function consLog() {
+        console.log('LABAS');
+    }
+
+    return (
+        <>
+            <button onClick={consLog} >TODAY</button>
+        </>
+    )
+}
--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -2,14 +2,16 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 import Cmr from './cmr'
+import TodayButton from '@/components/grafos/TodayButton'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      {/* <h1 className={`${styles.title} font-effect-fire-animation`}>CMR generator</h1> */}
-      <Cmr></Cmr>
+      <h1 className={`${styles.title} font-effect-fire-animation`}>CMR generator</h1>
+      <TodayButton/>
+      <Cmr/>
     </div>
   )
 }
